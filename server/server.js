0183ad17c553feda81746d8b13fdb394cf31a0e5
tanishsabane07 <tanish.sabane@gmail.com>
2026-04-23 10:08:21 +0530

    Fix Express 5 catch-all route for production

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ app.use("/applications", applicationRouter); // application router
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
